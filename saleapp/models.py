32f7019ede42ac6cdc47d8fe2512f239a856c34d
taobik <taobik>
2022-12-15 12:44:35 +0700

    code

--- saleapp/models.py
+++ saleapp/models.py
@@ -153,22 +153,22 @@ if __name__ == '__main__':
                   hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/trenduongbang_oh88do.jpg',
                   trang_thai=1, da_ban=35, so_luong_ton=234, the_loai_id=1, tac_gia_id=1, nha_xuat_ban_id=1)
         s2 = Sach(name='Vàng Xám Và Những Câu Chuyện Ngốc Nghếch', gia=108000,
-                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/trenduongbang_oh88do.jpg',
+                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/vangxamvanhungcauchuyenngocnghech_w1cc3j.jpg',
                   trang_thai=1, da_ban=175, so_luong_ton=221, the_loai_id=1, tac_gia_id=2, nha_xuat_ban_id=4)
         s3 = Sach(name='Nghĩa Tình Miền Tây', gia=140000,
-                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/trenduongbang_oh88do.jpg',
+                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/nghitrinhmientayjpg_ynwpvb.jpg',
                   trang_thai=1, da_ban=143, so_luong_ton=76, the_loai_id=1, tac_gia_id=3, nha_xuat_ban_id=5)
         s4 = Sach(name='Pokémon Diamond & Pearl: Bá Vương Ảo Ảnh Zoroark', gia=20000,
-                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/trenduongbang_oh88do.jpg',
+                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521999/pokemon-zoroark-ba-vuong-ao-anh_o3r4ti.jpg',
                   trang_thai=1, da_ban=64, so_luong_ton=238, the_loai_id=4, tac_gia_id=3, nha_xuat_ban_id=2)
         s5 = Sach(name='33 Chiến Lược Của Chiến Tranh (Tái Bản 2021)', gia=70000,
-                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/trenduongbang_oh88do.jpg',
+                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/33chienluocchientranh_h1nr6o.jpg',
                   trang_thai=1, da_ban=90, so_luong_ton=50, the_loai_id=3, tac_gia_id=4, nha_xuat_ban_id=1)
         s6 = Sach(name='Những Người Khổng Lồ Trong Giới Kinh Doanh', gia=100000,
-                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/trenduongbang_oh88do.jpg',
+                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/nguoikhonglotronggioikinhdoanh_vaaiig.jpg',
                   trang_thai=1, da_ban=164, so_luong_ton=322, the_loai_id=3, tac_gia_id=5, nha_xuat_ban_id=1)
         s7 = Sach(name='Thiên Tài Bên Trái, Kẻ Điên Bên Phải (Tái Bản 2021', gia=120000,
-                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/trenduongbang_oh88do.jpg',
+                  hinh_anh='https://res.cloudinary.com/dascseee2/image/upload/v1670521998/thientaitraikedienphai_qhxxj4.jpg',
                   trang_thai=1, da_ban=236, so_luong_ton=276, the_loai_id=2, tac_gia_id=6, nha_xuat_ban_id=6)
 
         db.session.add_all([s1, s2, s3, s4, s5, s6, s7])
